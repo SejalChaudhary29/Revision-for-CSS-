23558b49b23f3ea293d2f05b4f9c5876d5add2d6
= <=>
2024-04-04 10:55:16 +0530

    settimeout function

---
+++ Javascript/Javascript/setTimeOut.js
@@ -0,0 +1,24 @@
+
+
+setTimeout(()=>{
+    console.log('hello')
+
+}, 2000)
+
+
+function myName(){
+    console.log('hellloooo')
+}
+setTimeout(myName, 3000) 
+
+let count = 1
+setInterval(counter , 3000)
+
+ function counter(){
+    count++
+    console.log(count)
+ }
+
+ 
+ 
+
